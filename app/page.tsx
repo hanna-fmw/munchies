396b1a3a8f40567d0c4551fcd55b738ca00d0009
Hanna Hosk <hanna.hosk@gmail.com>
2024-05-06 19:43:50 +0200

    Added logic to fetch price ranges

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ type Restaurant = {
 	filter_ids: string[]
 	image_url?: string
 	delivery_time_minutes: number
-	price_range_id?: string
+	price_range_id: string
 }
 
 type Filter = {
@@ -32,6 +32,13 @@ type Hours = {
 	}
 }
 
+type PriceTier = {
+	priceTier: {
+		id: string
+		tier: string
+	}
+}
+
 export default function Home() {
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(true)
 	const [filteredRestaurants, setFilteredRestaurants] = useState<Restaurant[]>([])
@@ -40,9 +47,10 @@ export default function Home() {
 	const [restaurants, setRestaurants] = useState<Restaurant[]>([])
 	const [filters, setFilters] = useState<any>([])
 	const [deliveryTimeRange, setDeliveryTimeRange] = useState<string[]>([])
-
 	const [activeTimeRange, setActiveTimeRange] = useState<number | null>(null)
+
 	const [openingHours, setOpeningHours] = useState<Hours[]>([])
+	const [priceTiers, setPriceTiers] = useState<PriceTier[]>([])
 
 	const timeRanges = [
 		{ minTime: 0, maxTime: 10, label: '0-10 min' },
@@ -68,8 +76,9 @@ export default function Home() {
 		const res = await fetch('https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api/restaurants')
 		const data = await res.json()
 		if (data.restaurants.length > 0) {
-			data.restaurants.forEach((restaurant: any) => {
+			data.restaurants.forEach((restaurant: Restaurant) => {
 				getAllRestaurantsOpeningHours(restaurant.id)
+				getAllRestaurantsPriceTiers(restaurant.price_range_id)
 			})
 		}
 		setRestaurants(data.restaurants)
@@ -81,9 +90,22 @@ export default function Home() {
 		const data = await res.json()
 
 		console.log('opening hours', data)
-		//We put each openingHoursData object (which consists of restaurant_id and is_open) in an array:
+		//We put each "opening hours data" object (which consists
+		//of restaurant_id and is_open - as seen in swagger response) in an array:
 		//@ts-ignore
 		setOpeningHours((prevHours) => [...prevHours, { hours: { restaurant_id: data.restaurant_id, is_open: data.is_open } }])
+	}
+
+	// Function to fetch price ranges and update the state
+	const getAllRestaurantsPriceTiers = async (priceTierId: string) => {
+		const res = await fetch(`https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api/price-range/${priceTierId}`)
+		const data = await res.json()
+
+		console.log('price tiers', data)
+		//We put each "price ranges data" object (which consists
+		//of id and range - as seen in swagger response) in an array:
+		//@ts-ignore
+		setPriceTiers((prevPriceTiers) => [...prevPriceTiers, { priceTier: { id: data.id, tier: data.range } }])
 	}
 
 	const getAllFilters = async () => {
